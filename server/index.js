5bf7b200e09ec99c1b8dc42d365fdd1996760c25
notysozu <lifegamer2050@gmail.com>
2026-04-08 22:00:08 +0530

    cleanup(index.js): safe cleanup, removed comments, minor fixes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const { app } = require("./app");
 const connectDB = require("./data/db");
 
-// Boot
 const PORT = process.env.PORT || 4000;
 connectDB()
   .then(() => {
